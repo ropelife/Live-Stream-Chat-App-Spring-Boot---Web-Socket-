6f837091e6955060ef3cd6249089a05b3bafa3ba
Anjali Eknath Chaudhari <45571370+ropelife@users.noreply.github.com>
2024-03-20 12:32:16 -0400

    Add files via upload

---
+++ web-socket/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -0,0 +1,18 @@
+package com.example.websocket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.example.websocket.handler.WebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new WebSocketHandler(), "/websocket").setAllowedOrigins("*");
+    }
+}
